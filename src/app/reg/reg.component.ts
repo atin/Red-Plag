4e4b7e3162ff59be5b92f3c938efdbd33bb5795c
Atin Bainada <atinbainada1710@gmail.com>
2020-10-25 03:09:29 +0530

    create basic signup form

--- src/app/reg/reg.component.ts
+++ src/app/reg/reg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reg',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reg.component.scss']
 })
 export class RegComponent implements OnInit {
+  signup_form = this.fb.group({
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    pass: ['', Validators.required],
+    confirm_pass: [''],
+  }, { updateOn: 'submit' });
 
-  constructor() { }
+  hide = true;
+  errors: string[];
 
-  ngOnInit(): void {
+  getErrorMessages() {
+    let fn = this.signup_form.get('firstname');
+    let ln = this.signup_form.get('lastname');
+    let email = this.signup_form.get('email');
+    let pass = this.signup_form.get('pass');
+    this.errors = [];
+
+    fn.hasError('required') ? this.errors.push('First Name cannot be empty.') : '';
+    ln.hasError('required') ? this.errors.push('Last Name cannot be empty.') : '';
+    email.hasError('required') ? this.errors.push('Email cannot be empty.') : '';
+    email.hasError('email') ? this.errors.push('Not a valid email') : '';
+    pass.hasError('required') ? this.errors.push('Password cannot be empty.') : '';
   }
 
+  signup() {
+    this.getErrorMessages();
+  }
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {}
 }
