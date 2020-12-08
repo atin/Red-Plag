05d78b28373270ea80d29e86282eea5cd2fabc68
Koustav Sen <koustavsen17@gmail.com>
2020-12-08 14:56:08 +0530

    Removed Angular frontend

--- backend/user_profile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views as user_profile_views
-#from rest_framework.authtoken import views as authtoken_views
-urlpatterns = [
-    path('signup/', user_profile_views.registerUser, name='register-user'),
-    path('login/', user_profile_views.loginUser, name='login-user'),
-    path('upload', user_profile_views.uploadFiles, name='upload-files')
-]
--- src/app/models/errors.ts
+++
@@ -1,3 +0,0 @@
-export interface ErrorMessages {
-    [component: string]: string;
-}
