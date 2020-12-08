84a558e70abc582f40991ee917a7e59df584c483
Koustav Sen <koustavsen17@gmail.com>
2020-12-08 22:32:07 +0530

    Addded download function

--- Red_Plag/accounts/urls.py
+++ Red_Plag/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
     path('upload/', views.uploadfile, name="upload"),
-	path('algorithm/', views.initate, name="algorithm")
+	path('algorithm/', views.initate, name="algorithm"),
+	path('download/', views.filedownload, name="download"),
 ]
